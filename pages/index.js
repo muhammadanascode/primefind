db7852537904938262bf05593f0cf82b2c7c78e0
Muhammad Anas <anassohail34343@gmail.com>
2023-08-09 14:33:53 -0700

    Ui updated
    Created  a new Ui for prime find

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,47 @@
-import Head from "next/head"
-import Image from "next/image"
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>PrimeFind-Discover the ultimate selection of prime products </title>
+        <title>
+          PrimeFind-Discover the ultimate selection of prime products{" "}
+        </title>
       </Head>
-      <div className="flex justify-center my-4 mt-6">
-        <h1 className = "mb-4 text-xl font-extrabold leading-none tracking-wide text-pink-700 md:text-3xl lg:text-3xl dark:text-white trackin ">DISCOVER THE PRIME PRODUCTS</h1>
+      <div className="w-full m-auto mt-4 bg-gray-200 p-4 md:w-4/5 md:h-full md:flex">
+        {" "}
+        {/* Responsive classes */}
+        <div className="md:w-1/2 md:pr-4 ">
+          {" "}
+          {/* Responsive padding */}
+          <div className="p-4 md:p-8">
+            <p className="mt-4 tracking-widest font-bold text-5xl md:text-6xl bg-white w-32 md:w-48 px-2 md:px-4">
+              LET'S
+            </p>
+            <p className="mt-4 tracking-widest text-5xl md:text-6xl">EXPLORE</p>
+            <p className="mt-4 font-bold tracking-widest text-5xl  bg-yellow-500 w-72 md:text-6xl ">
+              UNIQUE
+            </p>
+            <p className="mt-4 tracking-widest text-6xl md:text-5xl">CLOTHES</p>
+            <p className="mt-4 text-sm md:text-base">
+            At PrimeFind, our ecommerce platform is your gateway to a world of exceptional products that cater to every facet of your needs and desires. We take you  to the latest fashion trends.
+            </p>
+          </div>
+          <div className="flex ">
+            <div className="w-1/4 h-1 bg-yellow-500"></div>
+            <button className=" w-1/8 bg-pink-500 rounded-md text-white p-2 -mt-4 hover:bg-pink-700">
+              <Link href={"/tshirts"}>Shop Now</Link>
+            </button>
+            <div className="w-1/4 h-1 bg-yellow-500"></div>
+          </div>
+        </div>
+        <div className="md:w-1/2 mt-4 md:mt-0">
+          {/* Image */}
+          <Image className="mt-2" src={"/girl2.png"} width={480} height={200} />
+        </div>
       </div>
-      <div className="flex justify-center mx-4" >
-        <Image src='/landingimg.png' width={800} height={450} alt="img" />
-      </div>
-      
     </>
-
-  )
+  );
 }
