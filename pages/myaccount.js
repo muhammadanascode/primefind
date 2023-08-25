2bbf071d0c785dfb328babae275a7f96c558e0ba
Muhammad Anas <anassohail34343@gmail.com>
2023-08-25 14:24:42 -0700

    condition in myaccount.js
    Validating conditions in my account.js has been updated

--- pages/myaccount.js
+++ pages/myaccount.js
@@ -29,10 +29,10 @@ const myaccount = () => {
         progress: undefined,
         theme: "light",
       });
-     return 
+      return;
     }
     setUserName(res.name);
-    setEmail(res.email); 
+    setEmail(res.email);
   }
 
   useEffect(() => {
@@ -49,8 +49,7 @@ const myaccount = () => {
   const [newPassword, setNewPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
 
-
-  //Updating state values 
+  //Updating state values
   const handleChange = (e) => {
     if (e.target.name === "username") {
       setUserName(e.target.value);
@@ -63,11 +62,77 @@ const myaccount = () => {
 
   //Function to submit the form
   const handleSubmit = async (e) => {
-
     e.preventDefault();
 
+    if (
+      (userName.length > 2 && newPassword.length > 2) ||
+      userName.length > 2
+    ) {
+      //If new password and confirm password arenot equal
+      if (newPassword === confirmPassword) {
+        const response = await fetch("/api/updateUser", {
+          method: "POST",
+          body: JSON.stringify({
+            name: userName,
+            password:newPassword,
+            email,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+        const res = await response.json();
+        console.log(res);
+        // If data successfully updated
+        if (res.success) {
+          toast.success("Credentials updated successfully", {
+            position: "top-center",
+            autoClose: 500,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+          });
+          //Removing jwt token from local storage
+          localStorage.removeItem("token");
+          //Redirecting user to login page
+          router.push("/login");
+          return;
+        }
+        //if any error occured in updating data
+        else {
+          toast.success(res.message, {
+            position: "top-center",
+            autoClose: 500,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+          });
+          return;
+        }
+      }
+      //If new password and confirm password arenot equal
+      else {
+        toast.error("New password and confirm password must be equal", {
+          position: "top-center",
+          autoClose: 500,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
+        return;
+      }
+    }
     //If username is less than 3 characters
-    if(!userName.length>2){
+    else {
       toast.error("Username must be of 3 characters", {
         position: "top-center",
         autoClose: 500,
@@ -78,83 +143,7 @@ const myaccount = () => {
         progress: undefined,
         theme: "light",
       });
-     return 
-    }
-
-    //If password length is less than 6 characters
-    else if(!newPassword.length>5){
-      toast.error("Password should contain 5 characters", {
-        position: "top-center",
-        autoClose: 500,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "light",
-      });
-     return 
-    }
-    
-    //If new password and confirm password arenot equal
-    else if(newPassword!==confirmPassword){
-      toast.error("New password and confirm password must be equal", {
-        position: "top-center",
-        autoClose: 500,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "light",
-      });
-     return 
-    }
-
-    //If the user enter the input correctly
-    const response = await fetch('/api/updateUser' , {
-      method:"POST",
-      body:JSON.stringify({name:userName , password:newPassword , email}),
-      headers:{
-        'Content-Type':'application/json'
-      }
-    })
-    const res = await response.json()
-    console.log(res);
-
-    // If data successfully updated
-    if(res.success){
-      toast.success("Credentials updated successfully", {
-        position: "top-center",
-        autoClose: 500,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "light",
-      });
-      //Removing jwt token from local storage
-      localStorage.removeItem('token')
-      //Redirecting user to login page
-      router.push('/login')
-
-      return 
-    }
-
-    //if any error occured in updating data
-    else{
-      toast.success(res.message, {
-        position: "top-center",
-        autoClose: 500,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "light",
-      });
-      return 
+      return;
     }
   };
 
@@ -215,7 +204,6 @@ const myaccount = () => {
             name="newpassword"
             onChange={handleChange}
             className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-            
           />
         </div>
         <div className="mb-6">
@@ -232,7 +220,6 @@ const myaccount = () => {
             name="confirmpassword"
             onChange={handleChange}
             className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-            
           />
         </div>
         <div className="flex items-start mb-6"></div>
