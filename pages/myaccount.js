2d522b89b54c0f1af42dbbf8a3208d1410ae40f7
Muhammad Anas <anassohail34343@gmail.com>
2023-08-25 13:56:53 -0700

    comments
    commmnets added in my account.js

--- pages/myaccount.js
+++ pages/myaccount.js
@@ -5,6 +5,7 @@ import { toast } from "react-toastify";
 const myaccount = () => {
   const router = useRouter();
 
+  //Function to fetch user data from api
   async function getUserdata() {
     const response = await fetch("/api/getUser", {
       method: "POST",
@@ -47,6 +48,8 @@ const myaccount = () => {
   const [newPassword, setNewPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
 
+
+  //Updating state values 
   const handleChange = (e) => {
     if (e.target.name === "username") {
       setUserName(e.target.value);
@@ -57,10 +60,12 @@ const myaccount = () => {
     }
   };
 
+  //Function to submit the form
   const handleSubmit = async (e) => {
 
     e.preventDefault();
 
+    //If username is less than 3 characters
     if(!userName.length>2){
       toast.error("Username must be of 3 characters", {
         position: "top-center",
@@ -75,6 +80,7 @@ const myaccount = () => {
      return 
     }
 
+    //If password length is less than 6 characters
     else if(!newPassword.length>5){
       toast.error("Password should contain 5 characters", {
         position: "top-center",
@@ -89,6 +95,7 @@ const myaccount = () => {
      return 
     }
     
+    //If new password and confirm password arenot equal
     else if(newPassword!==confirmPassword){
       toast.error("New password and confirm password must be equal", {
         position: "top-center",
@@ -102,6 +109,8 @@ const myaccount = () => {
       });
      return 
     }
+
+    //If the user enter the input correctly
     const response = await fetch('/api/updateUser' , {
       method:"POST",
       body:JSON.stringify({name:userName , password:newPassword , email}),
@@ -111,6 +120,8 @@ const myaccount = () => {
     })
     const res = await response.json()
     console.log(res);
+
+    // If data successfully updated
     if(res.success){
       toast.success("Credentials updated successfully", {
         position: "top-center",
@@ -122,11 +133,15 @@ const myaccount = () => {
         progress: undefined,
         theme: "light",
       });
+      //Removing jwt token from local storage
       localStorage.removeItem('token')
+      //Redirecting user to login page
       router.push('/login')
 
       return 
     }
+
+    //if any error occured in updating data
     else{
       toast.success(res.message, {
         position: "top-center",
